2b9117b63f37bb2135b7e33983423ec2c3f423d7
William Falk-Wallace <wfalkwallace@gmail.com>
2013-09-06 14:05:05 -0400

    formats Exception handler

--- TAFSException.java
+++ TAFSException.java
@@ -7,19 +7,21 @@
  *
  */
 public class TAFSException extends Exception {
-	 public TAFSException() {
-		 super();
-	 }
+	
+	public TAFSException() {
+		super();
+	}
 
-     public TAFSException(String message) {
-        super(message);
-     }
-     
-     public TAFSException(String message, Throwable cause) {
-         super(message, cause);
-     }
-     
-     public TAFSException(Throwable cause) {
-         super(cause);
-     }
+	public TAFSException(String message) {
+		super(message);
+	}
+
+	public TAFSException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public TAFSException(Throwable cause) {
+		super(cause);
+	}
+
 }
