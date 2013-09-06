7f5fa31494e7ce6cfdd2bf7f1487f7196e184092
William Falk-Wallace <wfalkwallace@gmail.com>
2013-09-06 15:58:32 -0400

    adds outline for user input handling

--- TAFSH.java
+++ TAFSH.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,8 @@ public class TAFSH {
 	 */
 	public static void main(String[] args) {
 		TAFS tafs = new TAFS();
-		
+		Scanner user = new Scanner(System.in);
+
 		System.out.println("\n\nWelcome to the TellApart File System (TAFS)\n" +
 				"===========================================\n" +
 				"Commands:\n" +
@@ -23,9 +26,31 @@ public class TAFSH {
 				"	move <source path> <target path> - move element\n" +
 				"	write <path> <content> - (over)write to text file" +
 				"	print - print system hierarchy\n\n");
-		
+
 		tafs.print();
-		
+
+		String[] cmd;
+		while(true) {
+			if(user.hasNextLine()){
+				cmd = user.nextLine().split(" ");
+				switch(cmd[0]) {
+				case "create":
+					break;
+				case "delete":
+					break;
+				case "move":
+					break;
+				case "write":
+					break;
+				case "print":
+					break;
+				default:
+					System.out.println("Illegal file system operation");
+				}
+			}
+		}
+
+
 		//TODO command handling/illegal fs op
 	}
 
