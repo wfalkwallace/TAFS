ee6c21d571459ed7f1e8c387eed971197ecbe770
William Falk-Wallace <wfalkwallace@gmail.com>
2013-09-06 12:55:18 -0400

    finishes create method

--- TAFS.java
+++ TAFS.java
@@ -11,43 +11,55 @@ import java.util.ArrayList;
 public class TAFS {
 
 	private ArrayList<Element> root = new ArrayList<Element>();
-	
+
 	//TODO illegal file system operation
 
-	
+
 	private void create(String type, String name, String path) {
 		//TODO Exceptions: path not found
-			String[] pathlist = path.split("/");
-			Element pwd = "root";
-			for(int i = 0; i < pathlist.length; i++){
-				for(Element e:pwd)
-					if(e.getName().equals(pathlist[i]))
-						pwdlist = pwd.getChildren();
-
-
-				
+		String[] pathlist = path.split("/");
+		try {
+			if(pathlist.length > 1) {
+				Element pwd;
+				for(Element e:root)
+					if(e.getName().equals(pathlist[0])) {
+						pwd = e;
+						break;
+					}
+				for(int i = 1; i < pathlist.length - 1; i++){
+					for(Element e:pwd.getChildren())
+						if(e.getName().equals(pathlist[i])) {
+							pwd = e;
+							break;
+						}
+				}
+				pwd.addChild(new Element(name, type, pwd));
 			}
-			root.get(root.indexOf(o))
-			Element elem = (Element) Class.forName(type).newInstance();
-			elem.setName(name);
-			
+			else
+				if(type.equals("drive"))
+					root.add(new Element(name));
+				else
+					throw new TAFSException("Illegal File System Operation");
+		} catch (TAFSException e) {
+			System.out.println(e.getMessage());
 		}
-		
 	}
-	
+
+
+
 	delete(String path) {
 		//TODO Exceptions: path not found
 	}
-	
+
 	move(String source, String target) {
 		//TODO Exceptions: path not found; path already exists
 
 	}
-	
+
 	writeToFile(String path, String contents) {
 		//TODO Exceptions: path not found; not a text file 
 	}
-	
+
 	//TODO search
 
 }
