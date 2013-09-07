779d48324aa8e828ba66d0d22a47569c4745026f
William Falk-Wallace <wfalkwallace@gmail.com>
2013-09-06 21:42:46 -0400

    small updates to shell switch

--- TAFSH.java
+++ TAFSH.java
@@ -22,7 +22,7 @@ public class TAFSH {
 				"Commands:\n" +
 				"	ll - print file system hierarchy\n" +
 				"	create <drive/folder/zip/text> <name> <path-of-parent> - create given entity\n" +
-				"	delete <d/f/z/t> <name> - remove directory/folder/zip/text\n" +
+				"	delete <path> - remove element\n" +
 				"	move <source path> <target path> - move element\n" +
 				"	write <path> <content> - (over)write to text file" +
 				"	print - print system hierarchy\n\n");
@@ -40,7 +40,7 @@ public class TAFSH {
 					tafs.create(arg[1], arg[2], arg[3]);
 					break;
 				case "delete":
-					tafs.
+					tafs.delete(arg[1]);
 					break;
 				case "move":
 					break;
